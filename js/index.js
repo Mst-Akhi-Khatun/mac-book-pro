b6df8b19326cd2b9464e6a19d3952bfd99a64ffc
Mst-Akhi-Khatun <abiraajmi@gmail.com>
2021-08-21 08:11:36 +0600

    done

--- js/index.js
+++ js/index.js
@@ -1,74 +1,50 @@
-// memory part
-function memory8Gb() {
-    const extraMemory = document.getElementById("extra-memory");
-    extraMemory.innerText = Number(0); 
-    totalPrice()   
-}
-function memory16Gb() {
-    const extraMemory = document.getElementById("extra-memory");
-    extraMemory.innerText = Number(180);
-    totalPrice()    
-}
 
-document.getElementById("8gb-memory").addEventListener('click', function() {    
-    memory8Gb();    
-});
-document.getElementById("16gb-memory").addEventListener('click', function() {    
-    memory16Gb();    
-});
+const bestPrice = document.getElementById("best-price");
+const memoryCost = document.getElementById("memory-cost");
+const storageCost = document.getElementById("storage-cost");
+const deliveryCost = document.getElementById("delivery-cost");
+const totalPrice = document.getElementById("total-price");
+const grandTotal = document.getElementById("grand-total");
+const inpuField = document.getElementById("code-field");
 
-
-// storage part
-
-function ssd256() {
-    const extraMemory = document.getElementById("extra-storage");
-    extraMemory.innerText = Number(0);    
-}
-function ssd512() {
-    const extraMemory = document.getElementById("extra-storage");
-    extraMemory.innerText = Number(100);    
-}
-function ssd1Tb() {
-    const extraMemory = document.getElementById("extra-storage");
-    extraMemory.innerText = Number(180);    
+// Updating Memory Price
+function updateMemoryPrice(amount) {
+    memoryCost.innerText = amount;
+    updateTotal();
+    updateGrandTotal();
 }
 
-document.getElementById("256-gb-ssd").addEventListener('click', function() {    
-    ssd256();    
-});
-document.getElementById("512-gb-ssd").addEventListener('click', function() {    
-    ssd512();    
-});
-document.getElementById("1tb-ssd").addEventListener('click', function() {    
-    ssd1Tb();    
-});
-
-
-// delivery part
-
-function deliveryCharge25() {
-    const extraMemory = document.getElementById("delivery-charge");
-    extraMemory.innerText = Number(0);    
-}
-function deliveryCharge21() {
-    const extraMemory = document.getElementById("delivery-charge");
-    extraMemory.innerText = Number(20);    
+// Updating Storage Price
+function updateStoragePrice(amount) {
+    storageCost.innerText = amount;
+    updateTotal();
+    updateGrandTotal();
 }
 
-document.getElementById("aug-25").addEventListener('click', function() {    
-    deliveryCharge25();    
-});
-document.getElementById("aug-21").addEventListener('click', function() {    
-    deliveryCharge21();    
-});
-
-// total price
-function totalPrice() {
-    const totalPrice = document.getElementById("total-price");
-    const bestPrice = document.getElementById("best-price").innerText;
-    const memoryPrice = document.getElementById("extra-memory").innerText;
-    const storagePrice = document.getElementById("extra-storage").innerText;
-    const deliveryCharge = document.getElementById("delivery-charge").innerText;
-    const totalPriceCount = Number(bestPrice + memoryPrice + storagePrice + deliveryCharge);
-    totalPrice = totalPriceCount;
+// Updating Delivery Cost
+function updateDeliveryCost(amount) {
+    deliveryCost.innerText = amount;
+    updateTotal();
+    updateGrandTotal();
 }
+
+// Updating Total Price
+function updateTotal() {
+    totalPrice.innerText = Number(bestPrice.innerText) + Number(memoryCost.innerText) + Number(storageCost.innerText) + Number(deliveryCost.innerText);
+}
+
+// Updating Grand Total Price
+function updateGrandTotal() {
+    grandTotal.innerText = Number(bestPrice.innerText) + Number(memoryCost.innerText) + Number(storageCost.innerText) + Number(deliveryCost.innerText);
+}
+
+// Updating Grand Total Price Using Promo-Code
+document.getElementById('promo-btn').addEventListener('click', function() {
+    const promoCode ="stevekaku";  //promo code
+    const discount = (grandTotal.innerText * 20) / 100; // getting discount
+    if (inpuField.value.toLowerCase() == promoCode.toLocaleLowerCase()) {
+        grandTotal.innerText = grandTotal.innerText - discount;
+    }
+    // clear input field
+    inpuField.value = '';
+})
